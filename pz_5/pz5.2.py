8dda60b13d589ee861ff56c2701045cfc332d952
dstroer113 <145475223+dstroer113@users.noreply.github.com>
2023-12-05 18:35:53 +0300

    pz5

--- pz_5/pz5.2.py
+++ pz_5/pz5.2.py
@@ -1,13 +1,13 @@
 import math
 
-
+#ввод функции
 def TrianglePS(a):
     P, S = (3 * a), (a**2 * math.sqrt(3/4))
     print("периметр:",P, "площадь:",S)
 
-
+#ввод данных
 a, b, c = input("Сторона первого треугольника:"), input("Сторона второго треугольника:"), input("Сторона третьего треугольника:")
-
+#обработка иключений 
 while type(a) != float:
     try:
         a = float(a)
@@ -26,5 +26,5 @@ while type(c) != float:
     except ValueError:
         print("Неправильно ввели!")
         c = input("Введите число: ")
-
+#вывод данных
 print("Первый треугольник:"), TrianglePS(a), print("Второй треугольник:"), TrianglePS(b),print("Третий треугольник:"), TrianglePS(c)
