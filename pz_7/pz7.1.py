095c650261aa4ccf6243af1609e0831a4619ea1f
dstroer113 <145475223+dstroer113@users.noreply.github.com>
2023-12-15 13:47:28 +0300

    pz7.1

--- pz_7/pz7.1.py
+++ pz_7/pz7.1.py
@@ -14,7 +14,7 @@ while type(n) != int:
         n = input("Введите число: ")
 # Функция возвращающая строку с числовым примером (случайные цифры и случайны знак либо + либо -)
 
-def RndExpr():
+def rndmdigits():
     lst_digit = list(numpy.random.choice(list(string.digits), n))
     lst_oper = list(numpy.random.choice(['+','-'], n-1))
     result = [None]*(2*n - 1)
@@ -22,5 +22,5 @@ def RndExpr():
     result[1::2] = lst_oper
     return "".join(result)
 # Использование функции и счет значения выражения с помощью функции eval
-s = RndExpr()
+s = rndmdigits()
 print(s, "=", eval(s))
