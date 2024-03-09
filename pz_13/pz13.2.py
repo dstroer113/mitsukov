f8602e399e246f8aeec8dac79e9b02ee591c054e
dstroer113 <145475223+dstroer113@users.noreply.github.com>
2024-03-09 14:34:26 +0300

    pz13.2.py

--- pz_13/pz13.2.py
+++ pz_13/pz13.2.py
@@ -1,23 +1,19 @@
 # В квадратной матрице все элементы, не лежащие на главной диагонали увеличить в 2 раза.
 import random
 
-def increase_elements(matrix):
-    n = len(matrix)
-    for i in range(n):
-        for j in range(n):
-            if i != j:
-                matrix[i][j] *= 2
-
-    return matrix
+def process_matrix(matrix):
+    size = len(matrix)
+    fmatrix = ([[(matrix[i][j]) * 2 if i != j else matrix[i][j] for j in range(size)] for i in range(size)])
+    return fmatrix
 
 
-n = random.randint(2, 6)
+n = random.randint(2, 3)
 print("Матрица:")
-matrix = [[random.randint(0, 100) for el in range(n)]for row in range(n)]
+matrix = [[random.randint(0, 10) for el in range(n)]for row in range(n)]
 for row in matrix:
     print(row)
 
-result = increase_elements(matrix)
+result = process_matrix(matrix)
 
 print("Измененная матрица:")
 for row in result:
