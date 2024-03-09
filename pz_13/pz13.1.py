5ef42ffba51d1883f06b286ca37dc72a39e7ae9c
dstroer113 <145475223+dstroer113@users.noreply.github.com>
2024-03-09 14:34:50 +0300

    pz13.1.py

--- pz_13/pz13.1.py
+++ pz_13/pz13.1.py
@@ -1,27 +1,15 @@
 # В матрице найти максимальный положительный элемент, кратный 4.
-
+from itertools import chain
 import random
 
-
-def max_positive_multiple_of_4(matrix):
-    max_num = None
-
-    for row in matrix:
-        for num in row:
-            if num > 0 and num % 4 == 0:
-                if max_num is None or num > max_num:
-                    max_num = num
-
-    return max_num
-
-
 n0, n1 = random.randint(2, 6), random.randint(2, 6)
-matrix = [[random.randint(0, 100) for el in range(n0)]for row in range(n1)]
+matrix = [[random.randint(0, 12) for el in range(n0)]for row in range(n1)]
 print("Матрица:")
 [print(i) for i in matrix]
-result = max_positive_multiple_of_4(matrix)
 
-if result is not None:
-    print(f"Максимальный положительный элемент, кратный 4: {result}")
+fmatrix = list(chain.from_iterable(matrix))
+max_num = max(filter(lambda x: x%4 == 0 and x > 0, fmatrix))
+if max_num is not None:
+    print(f"Максимальный положительный элемент, кратный 4: {max_num}")
 else:
     print("В матрице нет положительных элементов, кратных 4")
