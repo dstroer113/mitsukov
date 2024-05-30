c4bf3c4585720245b58b0ce0fc7ca79940f3188a
dstroer113 <145475223+dstroer113@users.noreply.github.com>
2024-05-30 21:51:27 +0300

    pz_17

---
+++ pz_17/pz17.2.py
@@ -0,0 +1,40 @@
+"""
+Разработать программу с применением пакета tk,
+взяв в качестве условия одну любую задачу из ПЗ 2-9
+"""
+
+
+import tkinter as tk
+
+
+def get_th(num):
+    if num > 999:
+        th_place = int(str(num)[-4])
+        return th_place
+    else:
+        return None
+
+
+def on_button_click():
+    num = int(entry.get())
+    th_digits = get_th(num)
+    if th_digits is not None:
+        label.config(text=f"Цифра в разряде тысяч: {th_digits}")
+    else:
+        label.config(text="Число меньше 1000")
+
+
+root = tk.Tk()
+root.title("Цифра в разряде тысяч")
+root.geometry("300x150")
+
+label = tk.Label(root, text="Введите число больше 999:")
+label.pack(pady=10)
+
+entry = tk.Entry(root)
+entry.pack(pady=5)
+
+button = tk.Button(root, text="Показать цифру", command=on_button_click)
+button.pack(pady=5)
+
+root.mainloop()
